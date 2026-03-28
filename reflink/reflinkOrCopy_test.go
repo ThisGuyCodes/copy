5ff7daf76d227a73ca87c9114a75c5e427919b47
Travis Johnson <travis@thisguy.codes>
2026-03-28 18:42:15 -0400

    add ReflinkOrCopy tests for APFS

---
+++ reflink/reflinkOrCopy_test.go
@@ -0,0 +1,58 @@
+package reflink_test
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/thisguycodes/copy/reflink"
+	"github.com/thisguycodes/copy/reflink/testutils/createmount"
+	"github.com/thisguycodes/copy/reflink/testutils/ts"
+)
+
+func TestReflinkOrCopyOnDarwinWithinAPFS(t *testing.T) {
+	ts.OnlyOn(t, "darwin_")
+	t.Parallel()
+
+	apfsMount := t.TempDir()
+	createmount.MountDiskImageMacOS(t, apfsMount, "APFS")
+
+	fileName := filepath.Join(apfsMount, "test.txt")
+
+	ts.NoErr(0, os.WriteFile(fileName, []byte("Hello, World!"), 0o644))(t)
+
+	fromFD := ts.NoErr(os.Open(fileName))(t)
+	toDirFD := ts.NoErr(os.Open(apfsMount))(t)
+	defer fromFD.Close()  // nolint:errcheck
+	defer toDirFD.Close() // nolint:errcheck
+
+	toName := "test-reflink.txt"
+
+	didReflink := ts.NoErr(reflink.ReflinkOrCopy(fromFD, toDirFD, toName))(t)
+	ts.Is(true)(t, didReflink)
+}
+
+func TestReflinkOrCopyOnDarwinAcrossAPFS(t *testing.T) {
+	ts.OnlyOn(t, "darwin_")
+	t.Parallel()
+
+	apfsMount1 := t.TempDir()
+	apfsMount2 := t.TempDir()
+
+	createmount.MountDiskImageMacOS(t, apfsMount1, "APFS")
+	createmount.MountDiskImageMacOS(t, apfsMount2, "APFS")
+
+	fileName := filepath.Join(apfsMount1, "test.txt")
+
+	ts.NoErr(0, os.WriteFile(fileName, []byte("Hello, World!"), 0o644))(t)
+
+	fromFD := ts.NoErr(os.Open(fileName))(t)
+	toDirFD := ts.NoErr(os.Open(apfsMount2))(t)
+	defer fromFD.Close()  // nolint:errcheck
+	defer toDirFD.Close() // nolint:errcheck
+
+	toName := "test-reflink.txt"
+
+	didReflink := ts.NoErr(reflink.ReflinkOrCopy(fromFD, toDirFD, toName))(t)
+	ts.Is(false)(t, didReflink)
+}
